a74291b9fa27a7ded350eaa5076ab34d7d049ba9
Kavin-nilaa <162492723+Kavin-nilaa@users.noreply.github.com>
2026-08-05 00:11:50 +0530

    Add Selenium drag and drop functionality in Task13.py
    Implement a Selenium script to drag and drop an element on a webpage.

--- Task13.py
+++ Task13.py
@@ -1,1 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://jqueryui.com/droppable/")
+
+actions = ActionChains(driver)
+
+frame = driver.find_element(By.XPATH,"//iframe[@class='demo-frame']")
+driver.switch_to.frame(frame)
+
+source = driver.find_element(By.ID,"draggable")
+target = driver.find_element(By.ID,"droppable")
+
+actions.drag_and_drop(source,target).perform() #to drag the source to the target
+
+driver.switch_to.default_content()
+driver.quit()
